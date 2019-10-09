6f93a49b0fe2a1efa4428c60391dc44ca029bbd7
evrenvural <evrenvural4@gmail.com>
2019-10-10 00:48:56 +0300

    main page designed without footer

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import CheckSvg from '../svg/check.svg';
+import GithubSvg from '../svg/github.svg';
+import MediumSvg from '../svg/medium.svg';
 
 export default class MyPage extends Component {
     render() {
@@ -8,7 +10,7 @@ export default class MyPage extends Component {
             <div className="my-page">
                 <Navbar />
 
-                <div className="attributes">
+                <div className="skills-and-school">
                     <div className="container">
 
                         <div className="attribute">
@@ -71,19 +73,107 @@ export default class MyPage extends Component {
                     </div>
 
                     <div className="experiences">
-                        <div className="lists">
-                            <h2 className="title">Relevant Experiences</h2>
+                        <div className="container">
+                            <div className="attribute">
+                                <div className="lists">
+                                    <h2 className="title">Relevant Experiences</h2>
 
-                            <div className="list">
-                                <img className="check" src={CheckSvg} alt="Check Icon"></img>
-                                <span className="text">asjsdsjaklhgda</span>
+                                    <div className="list">
+                                        <img className="check" src={CheckSvg} alt="Check Icon"></img>
+                                        <span className="text">asjsdsjaklhgda</span>
+                                    </div>
+
+                                </div>
                             </div>
-
                         </div>
+                    </div>
+
+                    <div className="projects">
+                        <div className="container">
+                            <div className="table">
+                                <div className="table-head">
+                                    <div className="container">
+                                        <img className="svg" src={GithubSvg} alt="Github" />
+                                        <h2 className="title">Open Source Projects</h2>
+                                    </div>
+                                </div>
+                                <div className="table-body">
+                                    <div className="datas">
+                                        <a href="https://www.google.com" target="_blank">
+                                            <div className="data">
+                                                <h2 className="title">stopwdsatch-football</h2>
+                                                <div className="text">Kronometre Futbol Oyunu is a nostalgic football game which can play on stopwatch</div>
+                                            </div>
+                                        </a>
+                                        <a href="https://www.google.com" target="_blank">
+                                            <div className="data">
+                                                <h2 className="title">stopwdsatch-football</h2>
+                                                <div className="text">Kronometre Futbol Oyunu is a nostalgic football game which can play on stopwatch</div>
+                                            </div>
+                                        </a>
+                                        <a href="https://www.google.com" target="_blank">
+                                            <div className="data">
+                                                <h2 className="title">stopwdsatch-football</h2>
+                                                <div className="text">Kronometre Futbol Oyunu is a nostalgic football game which can play on stopwatch</div>
+                                            </div>
+                                        </a>
+                                        <a href="https://www.google.com" target="_blank">
+                                            <div className="data">
+                                                <h2 className="title">stopwdsatch-football</h2>
+                                                <div className="text">Kronometre Futbol Oyunu is a nostalgic football game which can play on stopwatch</div>
+                                            </div>
+                                        </a>
+                                        <a href="https://www.google.com" target="_blank">
+                                            <div className="data">
+                                                <h2 className="title">stopwdsatch-football</h2>
+                                                <div className="text">Kronometre Futbol Oyunu is a nostalgic football game which can play on stopwatch</div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="writings">
+                        <div className="container">
+                            <div className="table">
+                                <div className="table-head">
+                                    <div className="container">
+                                        <img className="svg" src={MediumSvg} alt="Github" />
+                                        <h2 className="title">Articles</h2>
+                                    </div>
+                                </div>
+                                <div className="table-body">
+                                    <div className="datas">
+                                        <a href="https://www.google.com" target="_blank">
+                                            <div className="data">
+                                                <h2 className="title">stopwdsatch-football</h2>
+                                                <div className="text">Kronometre Futbol Oyunu is a nostalgic football game which can play on stopwatch</div>
+                                            </div>
+                                        </a>
+                                        <a href="https://www.google.com" target="_blank">
+                                            <div className="data">
+                                                <h2 className="title">stopwdsatch-football</h2>
+                                                <div className="text">Kronometre Futbol Oyunu is a nostalgic football game which can play on stopwatch</div>
+                                            </div>
+                                        </a>
+                                        <a href="https://www.google.com" target="_blank">
+                                            <div className="data">
+                                                <h2 className="title">stopwdsatch-football</h2>
+                                                <div className="text">Kronometre Futbol Oyunu is a nostalgic football game which can play on stopwatch</div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
 
-                )
-            }
-        }
+        )
+    }
+}
